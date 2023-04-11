ce77d8e593de3ec0f50122d6df8dd8089a1bad3f
rakeshbsw2022 <rakeshbsw2022@gmail.com>
2023-04-11 22:22:04 +0530

    all file commited

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(name="census-income",
+version="0.0.1",
+author="Rakesh",
+author_email="rakeshbsw2022@gmail.com",
+packages=find_packages(),
+install_requires=["pandas","numpy","flask"]
+)
